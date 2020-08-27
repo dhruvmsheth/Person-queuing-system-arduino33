a6ede1a775bfcae49c07dc97a6b80aa04e0363ce
Dhruv Sheth <67831664+dhruvsheth-ai@users.noreply.github.com>
2020-08-27 19:34:39 +0530

    Update entry-respond.cpp

--- entry-respond.cpp
+++ entry-respond.cpp
@@ -1,4 +1,3 @@
-// This model has to be deployed on an arduino 33 ble sense while one enters the mall
 /* Copyright Dhruv Sheth. Edited upon the initial person detction model trained by Tensorflow Authors
 Credits: Tensorflow Authors and Arduino Team
 
@@ -16,6 +15,15 @@ Credits: Tensorflow Authors and Arduino Team
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
+#include ESP32_Servo.h
+#include <WiFi.h>
+#include <WiFiMulti.h>
+WiFiMulti WiFiMulti;
+const char* ssid     = "JioFibertcz5n"; // Your SSID (Name of your WiFi)
+const char* password = "*******"; //Your Wifi password
+const char* host = "api.thingspeak.com";
+String api_key = "RYYSAR2DUGXAMBIC"; // Your API Key provied by thingspeak
+int count = 0;
 
 // Define hardware type, size, and output pins:
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
@@ -28,7 +36,8 @@ MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 void setup()
 {
   servo_8.attach(8);
-
+  Serial.begin(9600);
+    Connect_to_Wifi();
 }
 
   // Intialize the object:
@@ -37,6 +46,15 @@ void setup()
   myDisplay.setIntensity(0);
   // Clear the display:
   myDisplay.displayClear();
+}
+
+
+void loop(){
+
+ // call function to send data to Thingspeak
+  Send_Data();
+
+  delay(10);
 }
 
 // Flash the blue LED after each inference
@@ -69,6 +87,8 @@ void RespondToDetection(tflite::ErrorReporter* error_reporter,
     digitalWrite(LEDG, LOW);
     digitalWrite(LEDR, HIGH);
     servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
+    count++
+    
   } else {
     digitalWrite(LEDG, HIGH);
     digitalWrite(LEDR, LOW);
@@ -79,185 +99,121 @@ void RespondToDetection(tflite::ErrorReporter* error_reporter,
                        person_score, no_person_score);
 }                     // Running this inference 10 times to allow 10 people in and then setting a delay of 15min
 
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
-
-  if (person_score > no_person_score) {
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDR, HIGH);
-    servo_8.write(0);  // this servo moves to 0degrees in order to open the mall door when a person is detected to ensure no touch entry system
-  } else {
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDR, LOW);
-    servo_8.write(180); // this servo moves to 180degrees when no person is detected
-  }
-
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
-}
 
 // Now we have let in 10people inside the store, so we set a delay of 15min wait time for others waiting outside to let them in 
 // Displaying wait time on the screen every 1min
 
-
- myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 15min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 14min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 13min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 12min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 11min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 10min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 9min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 8min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 7min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 6min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 5min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 4min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 3min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 2min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Waiting 1min");
- delay(60000);
-  myDisplay.setTextAlignment(PA_CENTER);
- myDisplay.print("Go ");
- delay(1000);
+ if(count = 10)  // when 10 people are detected, we instruct it to start displaying wait time to other people to wait until 15 min 
+   myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 15min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 14min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 13min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 12min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 11min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 10min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 9min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 8min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 7min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 6min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 5min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 4min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 3min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 2min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Waiting 1min");
+   delay(60000);
+    myDisplay.setTextAlignment(PA_CENTER);
+   myDisplay.print("Go ");
+  delay(1000);
+  (count = 0); //the people count is again reset to 0 to repeat the action again
  // Finally they are allowed to go inside the store after 15min
 
+
+ void Connect_to_Wifi()
+{
+
+  // We start by connecting to a WiFi network
+  WiFiMulti.addAP(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(10);
+  }
+
+  SerialSerial.println("Prepare to send data");
+.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void Send_Data()
+{
+
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+
+  const int httpPort = 80;
+
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  else
+  {
+    String data_to_send = api_key;
+    data_to_send += "&field1=";
+    data_to_send += String(count);
+    data_to_send += "\r\n\";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + api_key + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(data_to_send.length());
+    client.print("\n\n");
+    client.print(data_to_send);
+
+    delay(10);  // reduced delay to perform real time data collection
+  }
+
+  client.stop();
+
+}
 
 #endif  // ARDUINO_EXCLUDE_CODE
